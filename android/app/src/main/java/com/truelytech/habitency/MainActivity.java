db6c2241539faa383088bae789de63829767b000
rajdeepkumar200 <rajdeepkumar200@users.noreply.github.com>
2026-01-08 01:41:25 +0530

    Keep app in RAM to prevent refresh when switching apps
    - Add largeHeap and hardwareAccelerated flags to prevent memory clearing
    
    - Enable alwaysRetainTaskState to preserve app state when backgrounded
    
    - Save and restore WebView state to prevent page refresh
    
    - Keep WebView timers running in onPause to maintain Pomodoro countdown
    
    - Enable DOM storage and database for state persistence

--- android/app/src/main/java/com/truelytech/habitency/MainActivity.java
+++ android/app/src/main/java/com/truelytech/habitency/MainActivity.java
@@ -2,6 +2,7 @@ package com.truelytech.habitency;
 
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.truelytech.habitency.plugins.UpdateInstallerPlugin;
 
@@ -10,5 +11,40 @@ public class MainActivity extends BridgeActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		registerPlugin(UpdateInstallerPlugin.class);
+		
+		// Keep WebView in memory and prevent it from being destroyed
+		if (this.bridge != null && this.bridge.getWebView() != null) {
+			WebView webView = this.bridge.getWebView();
+			webView.getSettings().setDomStorageEnabled(true);
+			webView.getSettings().setDatabaseEnabled(true);
+			webView.setWebContentsDebuggingEnabled(true);
+		}
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// Save WebView state to prevent refresh
+		if (this.bridge != null && this.bridge.getWebView() != null) {
+			this.bridge.getWebView().saveState(outState);
+		}
+	}
+	
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		// Restore WebView state
+		if (this.bridge != null && this.bridge.getWebView() != null && savedInstanceState != null) {
+			this.bridge.getWebView().restoreState(savedInstanceState);
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Don't pause WebView timers to keep Pomodoro running
+		if (this.bridge != null && this.bridge.getWebView() != null) {
+			this.bridge.getWebView().onResume();
+		}
 	}
 }
